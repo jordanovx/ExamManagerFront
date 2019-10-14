5dcf53d14af1b8cc27d8cceffec0eb7c7e0c6e4d
jordanovx <jordanov2295@gmail.com>
2019-10-14 22:44:29 +0200

    Fix  v1

--- src/pages/TeachersPage.js
+++ src/pages/TeachersPage.js
@@ -5,42 +5,40 @@ import { Table } from "react-bootstrap";
 
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 class TeachersPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: false };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { data : false}
-    }
+  getTeachers() {
+    fetch("http://localhost:8080/professors", {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result);
+          this.setState({ data: Object.values(result) });
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
 
-    getTeachers() {
-        fetch("http://localhost:8080/professors", {
-            mode: "cors",
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            }
-        })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    this.setState({data : Object.values(result)});
-                },
-                error => {
-                    console.log(error);
-                }
-            );
-    }
+  componentDidMount() {
+    console.log(this.getTeachers());
+  }
 
-    componentDidMount() {
-        console.log(this.getTeachers());
-    }
-
-    render() {
+  render() {
     return (
       <div>
-        <NavigationBar />
         <Breadcrumb className="troski">
           <Breadcrumb.Item href="/">Почетна</Breadcrumb.Item>
           <Breadcrumb.Item href="/teachers">Професори</Breadcrumb.Item>
@@ -57,13 +55,19 @@ class TeachersPage extends React.Component {
                 </tr>
               </thead>
               <tbody>
-              {(this.state.data) ?  this.state.data.map((item, i) => <tr> <td> {i+1}</td> <td> {item.name}</td> <td> {item.surname}</td></tr>) : ""}
-
+                {this.state.data
+                  ? this.state.data.map((item, i) => (
+                      <tr>
+                        {" "}
+                        <td> {i + 1}</td> <td> {item.name}</td>{" "}
+                        <td> {item.surname}</td>
+                      </tr>
+                    ))
+                  : ""}
               </tbody>
             </Table>
           </div>
         </div>
-        <Footer />
       </div>
     );
   }
