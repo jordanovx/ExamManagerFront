f585b710af0f7d796ad1f721592a6d13f958d4a5
jordanovx <jordanov2295@gmail.com>
2020-01-28 13:24:52 +0100

    Responsive design, mobile friendly

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Row, Col,  Container } from "react-bootstrap";
+import { Navbar, Nav, Row, Col, Container } from "react-bootstrap";
 import img from "../images/logo_finki.png";
 import NavDropdown from "react-bootstrap/NavDropdown";
 class NavigationBar extends React.Component {
@@ -14,33 +14,49 @@ class NavigationBar extends React.Component {
       >
         <Container>
           <Row>
-            <Col xs={2}>
-              <a href="/"> <img src={img} alt="logo" height="35" width="35"/> </a>
+            <Col>
+              <a href="/" className="finki-logo">
+                {" "}
+                <img src={img} alt="logo" height="55" width="55" />{" "}
+              </a>
             </Col>
             <Col xs={10}>
               <Nav>
                 <Nav.Link href="/"> Почетна </Nav.Link>
                 <NavDropdown title="Професори" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="/teachers">Преглед</NavDropdown.Item>
-                  <NavDropdown.Item href="/addteachers">Додади</NavDropdown.Item>
+                  <NavDropdown.Item href="/addteachers">
+                    Додади
+                  </NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Простории" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="/classrooms">Преглед</NavDropdown.Item>
-                  <NavDropdown.Item href="/addclassroom">Додади</NavDropdown.Item>
+                  <NavDropdown.Item href="/classrooms">
+                    Преглед
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/addclassroom">
+                    Додади
+                  </NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Предмети" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="/subjects">Преглед</NavDropdown.Item>
-                  <NavDropdown.Item href="/addsubjects">Додади</NavDropdown.Item>
+                  <NavDropdown.Item href="/addsubjects">
+                    Додади
+                  </NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Распоред" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="/timetable">Преглед</NavDropdown.Item>
-                  <NavDropdown.Item href="/addtimetable">Додади</NavDropdown.Item>
+                  <NavDropdown.Item href="/addtimetable">
+                    Додади
+                  </NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Сесии" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="/session">Преглед</NavDropdown.Item>
                   <NavDropdown.Item href="/addsession">Додади</NavDropdown.Item>
                 </NavDropdown>
-            </Nav>
+                <Nav.Link href="\login" className="najava">
+                  Најава
+                </Nav.Link>
+              </Nav>
             </Col>
           </Row>
         </Container>
