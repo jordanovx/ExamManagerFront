d595be73e1e64f5da5acc3723b0fc5b9743f1211
jordanovx <jordanov2295@gmail.com>
2020-01-30 03:04:47 +0100

    Mobile, Tablet, Desktop

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,40 +6,39 @@ class Login extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="loginWrapper">
-          <Form>
-            <Form.Group controlId="formGroupEmail">
-              <Form.Label>Адреса</Form.Label>
-              <Form.Control type="email" placeholder="Адреса" />
-            </Form.Group>
-            <Form.Group controlId="formGroupPassword">
-              <Form.Label>Лозинка</Form.Label>
-              <Form.Control type="password" placeholder="Лозинка" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Check type="checkbox" label="Запомни ме?" />
-            </Form.Group>
-            <Form.Group className="row">
-              <div className="col-6">
-                <Button
-                  className="btn btn-block"
-                  variant="outline-primary"
-                  type="btn-primary"
-                >
-                  Најави се
-                </Button>
-              </div>
-              <div className="col-6">
-                <Button
-                  className="btn btn-block"
-                  type="btn-secondary"
-                  href="/signup"
-                >
-                  Регистрирај се!
-                </Button>
-              </div>
-            </Form.Group>
-          </Form>
+        <div className="wrapper">
+          <div className="loginWrapper">
+            <Form>
+              <Form.Group controlId="formGroupEmail">
+                <Form.Label>Адреса</Form.Label>
+                <Form.Control type="email" placeholder="Адреса" />
+              </Form.Group>
+              <Form.Group controlId="formGroupPassword">
+                <Form.Label>Лозинка</Form.Label>
+                <Form.Control type="password" placeholder="Лозинка" />
+              </Form.Group>
+              <Form.Group>
+                <Form.Check type="checkbox" label="Запомни ме?" />
+              </Form.Group>
+              <Form.Group className="row">
+                <div className="col-6">
+                  <Button className="btn btn-block" type="btn-primary">
+                    Најави се
+                  </Button>
+                </div>
+                <div className="col-6">
+                  <Button
+                    className="btn btn-block"
+                    variant="outline-primary"
+                    type="btn-secondary"
+                    href="/signup"
+                  >
+                    Регистрирај се!
+                  </Button>
+                </div>
+              </Form.Group>
+            </Form>
+          </div>
         </div>
       </div>
     );
